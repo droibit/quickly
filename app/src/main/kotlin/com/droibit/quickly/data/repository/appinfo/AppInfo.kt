db89c4deb0e70f0f187c9fdbd0dd1f558ae8c56f
Shinya Kumagai <roomful.rooms@gmail.com>
2016-10-31 14:14:30 +0900

    Implement AppInfo

--- app/src/main/kotlin/com/droibit/quickly/data/repository/appinfo/AppInfo.kt
+++ app/src/main/kotlin/com/droibit/quickly/data/repository/appinfo/AppInfo.kt
@@ -1,5 +1,15 @@
 package com.droibit.quickly.data.repository.appinfo
 
+import com.github.gfx.android.orma.annotation.Column
+import com.github.gfx.android.orma.annotation.Setter
+import com.github.gfx.android.orma.annotation.Table
 
-class AppInfo {
-}
+@Table
+data class AppInfo @Setter constructor(
+        @Column(unique = true) val packageName: String,
+        @Column(indexed = true) val name: String,
+        @Column val versionName: String,
+        @Column val versionCode: Int,
+        @Column val preInstalled: Boolean,
+        @Column(indexed = true) val lastUpdateTime: Long
+)
