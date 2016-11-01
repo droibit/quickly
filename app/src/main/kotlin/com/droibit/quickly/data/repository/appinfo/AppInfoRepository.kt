5a8f12da00d4840988f1a38f52a292300d8d0bb1
Shinya Kumagai <roomful.rooms@gmail.com>
2016-11-01 15:27:29 +0900

    Updated AppInfoRepository interface

--- app/src/main/kotlin/com/droibit/quickly/data/repository/appinfo/AppInfoRepository.kt
+++ app/src/main/kotlin/com/droibit/quickly/data/repository/appinfo/AppInfoRepository.kt
@@ -1,4 +1,15 @@
 package com.droibit.quickly.data.repository.appinfo
 
+import rx.Observable
+import rx.Single
+
 interface AppInfoRepository {
+
+    fun getAll(): Observable<AppInfo>
+
+    fun addAll(appInfoList: List<AppInfo>): Observable<AppInfo>
+
+    fun update(appInfo: AppInfo): Single<Boolean>
+
+    fun delete(packageName: String): Single<Boolean>
 }
