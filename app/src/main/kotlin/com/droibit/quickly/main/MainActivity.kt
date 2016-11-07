3a62daf8f2fe3488aa9c1e7e8fd9686fae070f01
Shinya Kumagai <roomful.rooms@gmail.com>
2016-11-07 14:02:34 +0900

    Implement layout of main screen

--- app/src/main/kotlin/com/droibit/quickly/main/MainActivity.kt
+++ app/src/main/kotlin/com/droibit/quickly/main/MainActivity.kt
@@ -2,9 +2,19 @@ package com.droibit.quickly.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ProgressBar
 import com.droibit.quickly.R
+import com.github.droibit.chopstick.bindView
 
 class MainActivity : AppCompatActivity() {
+
+    private val recyclerView: RecyclerView by bindView(R.id.list)
+
+    private val progressBar: ProgressBar by bindView(R.id.progress)
+
+    private val emptyView: View by bindView(R.id.empty)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
