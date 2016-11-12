6c62942d58a95aa8200ad2d167b83fa2ac31f9d6
Shinya Kumagai <roomful.rooms@gmail.com>
2016-11-12 16:37:21 +0900

    Fixed ProviderModule

--- app/src/main/kotlin/com/droibit/quickly/data/repository/RepositoryModule.kt
+++ app/src/main/kotlin/com/droibit/quickly/data/repository/RepositoryModule.kt
@@ -1,8 +1,21 @@
 package com.droibit.quickly.data.repository
 
+import com.droibit.quickly.data.repository.appinfo.AppInfoRepository
+import com.droibit.quickly.data.repository.appinfo.AppInfoRepositoryImpl
+import com.droibit.quickly.data.repository.source.AppInfoDataSource
+import com.droibit.quickly.data.repository.source.AppInfoDataSourceImpl
 import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.singleton
 
 fun repositoryModule() = Kodein.Module {
 
+    bind<AppInfoDataSource>() with singleton { AppInfoDataSourceImpl(pm = instance()) }
 
+    bind<AppInfoRepository>() with singleton {
+        AppInfoRepositoryImpl(
+                orma = instance(),
+                appInfoSource = instance()
+        )
+    }
 }
