53b854f2dc82b9d2989aa7529ef9634b4409e18f
Shinya Kumagai <roomful.rooms@gmail.com>
2016-11-14 08:45:49 +0900

    Added MainContract

--- app/src/main/kotlin/com/droibit/quickly/main/MainContract.kt
+++ app/src/main/kotlin/com/droibit/quickly/main/MainContract.kt
@@ -1,7 +1,53 @@
 package com.droibit.quickly.main
 
-/**
- * Created by kumagai on 2016/11/13.
- */
+import android.support.annotation.IdRes
+import com.droibit.quickly.R
+import com.droibit.quickly.data.repository.appinfo.AppInfo
+import rx.Observable
+
 interface MainContract {
+
+    enum class MenuItem(@IdRes val id: Int) {
+        REFRESH(R.id.refresh),
+        SORT_BY(R.id.sort_by),
+        SHOW_SYSTEM(R.id.show_system),
+        SETTINGS(R.id.settings);
+
+        companion object {
+            @JvmStatic
+            fun from(@IdRes id: Int) = values().first { it.id == id }
+        }
+    }
+
+    interface View {
+
+        fun showAppInfoList(appInfoList: List<AppInfo>)
+
+        fun showNoAppInfo()
+
+        fun setLoadingIndicator(active: Boolean)
+    }
+
+    interface Navigator {
+
+        fun navigateSettings()
+    }
+
+    interface Presenter {
+
+        fun onCreate(shouldLoad: Boolean)
+
+        fun onResume()
+
+        fun onPause()
+
+        fun onOptionsItemClicked(menuItem: MenuItem)
+    }
+
+    interface LoadTask {
+
+        fun isRunning(): Observable<Boolean>
+
+        fun load(forceReload: Boolean): Observable<List<AppInfo>>
+    }
 }
