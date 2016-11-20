218460178d198d7fa87d10d64b431e265199f13e
Shinya Kumagai <roomful.rooms@gmail.com>
2016-11-20 13:59:19 +0900

    Implement RxBus

--- app/src/main/kotlin/com/droibit/quickly/data/provider/eventbus/RxBus.kt
+++ app/src/main/kotlin/com/droibit/quickly/data/provider/eventbus/RxBus.kt
@@ -1,7 +1,19 @@
 package com.droibit.quickly.data.provider.eventbus
 
-/**
- * Created by kumagai on 2016/10/31.
- */
+import com.jakewharton.rxrelay.BehaviorRelay
+import rx.Observable
+
+
 class RxBus {
+
+    object Nothing
+
+    private val subject: BehaviorRelay<Any> = BehaviorRelay.create()
+
+    fun asObservable(): Observable<Any> {
+        return subject.filter { it != Nothing }
+                .doOnNext { subject.call(Nothing) }
+    }
+
+    fun call(value: Any) = subject.call(value)
 }
