e1e538216d56e892aacbce5779db791b6def9798
Shinya Kumagai <roomful.rooms@gmail.com>
2016-11-23 18:47:32 +0900

    Implemented Parcelable

--- app/src/main/kotlin/com/droibit/quickly/data/repository/appinfo/AppInfo.kt
+++ app/src/main/kotlin/com/droibit/quickly/data/repository/appinfo/AppInfo.kt
@@ -1,5 +1,7 @@
 package com.droibit.quickly.data.repository.appinfo
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.github.gfx.android.orma.annotation.Column
 import com.github.gfx.android.orma.annotation.PrimaryKey
 import com.github.gfx.android.orma.annotation.Setter
@@ -13,5 +15,36 @@ data class AppInfo @Setter constructor(
         @Column val versionCode: Int,
         @Column val icon: Int, // resource Id
         @Column val preInstalled: Boolean,
-        @Column val lastUpdateTime: Long
-)
+        @Column val lastUpdateTime: Long) : Parcelable {
+
+    companion object {
+
+        @JvmField
+        val CREATOR: Parcelable.Creator<AppInfo> = object : Parcelable.Creator<AppInfo> {
+            override fun createFromParcel(source: Parcel): AppInfo = AppInfo(source)
+            override fun newArray(size: Int): Array<AppInfo?> = arrayOfNulls(size)
+        }
+    }
+
+    constructor(source: Parcel) : this(
+            source.readString(),
+            source.readString(),
+            source.readString(),
+            source.readInt(),
+            source.readInt(),
+            1 == source.readInt(),
+            source.readLong()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(packageName)
+        dest.writeString(name)
+        dest.writeString(versionName)
+        dest.writeInt(versionCode)
+        dest.writeInt(icon)
+        dest.writeInt((if (preInstalled) 1 else 0))
+        dest.writeLong(lastUpdateTime)
+    }
+}
