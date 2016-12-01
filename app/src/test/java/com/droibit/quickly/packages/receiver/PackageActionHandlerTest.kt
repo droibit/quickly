4541cd2015184dada84108b4ee39e41d877da059
Shinya Kumagai <roomful.rooms@gmail.com>
2016-12-01 22:22:00 +0900

    Added PackageActionHandlerTest

---
+++ app/src/test/java/com/droibit/quickly/packages/receiver/PackageActionHandlerTest.kt
@@ -0,0 +1,64 @@
+package com.droibit.quickly.packages.receiver
+
+import com.droibit.quickly.packages.PackageContract
+import com.droibit.quickly.packages.PackageContract.Action.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.junit.MockitoJUnit
+
+class PackageActionHandlerTest {
+
+    @Rule
+    @JvmField
+    val mockito = MockitoJUnit.rule()
+
+    @Mock
+    private lateinit var receiver: PackageContract.Receiver
+
+    private lateinit var handler: PackageActionHandler
+
+    @Before
+    fun setUp() {
+        handler = PackageActionHandler(receiver)
+    }
+
+    @Test
+    fun onPackageAdded_startPackageAction() {
+        val packageName = "add"
+        handler.onPackageAdded(packageName)
+
+        verify(receiver).startPackageAction(PACKAGE_ADDED, packageName)
+    }
+
+    @Test
+    fun onPackageReplaced_startPackageAction() {
+        val packageName = "replaced"
+        handler.onPackageReplaced(packageName)
+
+        verify(receiver).startPackageAction(PACKAGE_REPLACED, packageName)
+    }
+
+    @Test
+    fun onPackageRemoved_startPackageAction() {
+        // not replacing
+        run {
+            val packageName = "not_replacing"
+            handler.onPackageRemoved(packageName, replacing = false)
+
+            verify(receiver).startPackageAction(PACKAGE_REMOVED, packageName)
+        }
+        reset(receiver)
+
+        // replacing
+        run {
+            val packageName = "not_replacing"
+            handler.onPackageRemoved(packageName, replacing = true)
+
+            verify(receiver, never()).startPackageAction(PACKAGE_REMOVED, packageName)
+        }
+    }
+
+}
